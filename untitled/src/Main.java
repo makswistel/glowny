3a3aa4455dcf72a267fe8c74b79cc51034e0c80c
sznusiek <m.wis99@wp.pl>
2023-09-28 10:49:08 +0200

    Java

---
+++ untitled/src/Main.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+       // int i = 2;
+        //system.out.println(i);
+        //double f = 2.0;
+       // long l - 2L;
+
+        //Integer i2 = null;
+        //System.out.println(i2.doubleValue());
+        //Double f2 = 2.0;
+        //Long l2 = 2L;
+
+
+        //char w = 'X';
+        //String name = "21savage test oplerków";
+        //System.out.println(name.toLowerCase());
+        //String name1 = "21savage test oplerków";
+        //System.out.println(name1.toUpperCase());
+
+        int i = 1;
+        for (i=1;i<=15;++i)
+        {
+          //System.out.print(i + ",");
+
+        }
+        int a=16;
+                while(a<=30)
+                {
+                    //System.out.print(a + ",");
+                    a++;
+
+                }
+                int y = 0;
+                int x = 10;
+                String nazwa = "Hello";
+                while (y <= 9) {
+                    System.out.print(y++ + ", ");
+                    if(y>9)
+                        System.out.println(x);
+
+                }
+                System.out.println(nazwa);
+        a();
+        b();
+        c();
+    }
+
+        public static void a() {
+            System.out.println("Pessi");
+
+        }
+
+        protected static void b(){
+            System.out.println("Penaldo");
+        }
+
+        private static void c(){
+            System.out.println("Peymar Jr");
+        }
+
+
+
+
+
+
+    }
+
